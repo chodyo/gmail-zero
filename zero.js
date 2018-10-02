751793ed79f07e18edd0ef2b592454f9d7371a81
Cody Heffner <4218016+chody-h@users.noreply.github.com>
2018-10-02 00:07:35 -0400

    First commit

---
+++ zero.js
@@ -0,0 +1,25 @@
+const POLL = 1000;
+const SUN_SRC =
+  "//ssl.gstatic.com/bt/C3341AA7A1A076756462EE2E5CD71C11/zero/1x/ic_zero_inbox.png";
+const SUN = `<img alt="No new mail!" src=${SUN_SRC}>`;
+const BACKGROUND = "#7baaf7";
+
+function checkZeroInbox() {
+  function getTableRow() {
+    let elements = document.getElementsByTagName("td");
+    for (let e in elements) {
+      if (elements[e].className == "TC") {
+        chrome.extension.sendRequest({}, function(response) {});
+        return elements[e];
+      }
+    }
+  }
+
+  let row = getTableRow();
+  if (row && row.innerHTML === "No new mail!") {
+    row.innerHTML = SUN;
+    row.style.backgroundColor = BACKGROUND;
+  }
+}
+
+setInterval(checkZeroInbox, POLL);
