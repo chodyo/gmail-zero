8f7d11fffc0bde66ff6381acccc8d563dc642d70
Cody Heffner <4218016+chodyo@users.noreply.github.com>
2023-12-19 07:50:19 -0500

    v3

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # gmail-zero
+
 Brings Google's Inbox-zero theme to Gmail.
 
 ![what it looks like](/images/ic_zero_inbox_screenshot.png)
--- zero.js
+++ zero.js
@@ -4,17 +4,17 @@ const SUN_SRC =
 const SUN = `<img alt="No new mail!" src=${SUN_SRC}>`;
 const BACKGROUND = "#7baaf7";
 
-function checkZeroInbox() {
-  function getTableRow() {
-    let elements = document.getElementsByTagName("td");
-    for (let e in elements) {
-      if (elements[e].className == "TC") {
-        chrome.extension.sendRequest({}, function(response) {});
-        return elements[e];
-      }
+function getTableRow() {
+  let elements = document.getElementsByTagName("td");
+  for (let e in elements) {
+    if (elements[e].className == "TC") {
+      chrome.extension.sendRequest({}, function (response) {});
+      return elements[e];
     }
   }
+}
 
+function checkZeroInbox() {
   let row = getTableRow();
   if (row && row.innerHTML === "No new mail!") {
     row.innerHTML = SUN;
